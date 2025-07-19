e57702b943570fdd3e0862b3300f1c20c518b181
kaushal kishor <stardust@niit.localdomain>
2025-07-19 14:33:43 +0530

    init project

---
+++ src/main/java/com/kkdevprotal/SpringRedisBridge/SpringRedisBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.kkdevprotal.SpringRedisBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkdevprotal/SpringRedisBridge/SpringRedisBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkdevprotal.SpringRedisBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
